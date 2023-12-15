b43bb5ce809f1ee19db002bd4aaa662330628e61
hung1720 <hungtdqse140575@fpt.edu.vn>
2023-12-15 23:37:43 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AspectRatio from './pages/AspectRatio';
+
+function App() {
+  return (
+    <>
+      <AspectRatio />
+    </>
+  )
+}
+export default App;
+
---
+++ src/pages/AspectRatio.jsx
@@ -0,0 +1,16 @@
+function AspectRatio() {
+    return (
+        <>
+            <iframe
+                className='w-full aspect-square'
+                src="https://www.youtube.com/embed/Pys2iOT9rpw?list=RDPys2iOT9rpw"
+                title="WEAN – shhhhhhh.. feat tlinh (Official Lyrics Video)"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+            >
+            </iframe>
+        </>
+    )
+}
+export default AspectRatio;
